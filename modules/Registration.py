c83df1bdf15b244a798317027719bab4cd55fa00
Volkov <seishikage@gmail.com>
2021-02-28 14:02:19 +0300

    Основные заполняемые окна при регистрации нового пользователя

--- modules/Registration.py
+++ modules/Registration.py
@@ -12,3 +12,26 @@ class Registration(QMainWindow):
 
         self.setWindowTitle("Регистрация")
         self.setGeometry(700, 450, 300, 100)
+
+        self.general_layout = QVBoxLayout()
+
+        self.login = QLineEdit()
+        self.general_layout.addWidget(self.login)
+
+        self.password_one = QLineEdit()
+        self.general_layout.addWidget(self.password_one)
+        self.password_one.setEchoMode(QLineEdit.Password)
+
+        self.password_two = QLineEdit()
+        self.general_layout.addWidget(self.password_two)
+        self.password_two.setEchoMode(QLineEdit.Password)
+
+        self.btn_register = QPushButton("Зарегистрироваться")
+        self.general_layout.addWidget(self.btn_register)
+
+        self.btn_return = QPushButton("Вернуться")
+        self.general_layout.addWidget(self.btn_return)
+
+        widget = QWidget()
+        widget.setLayout(self.general_layout)
+        self.setCentralWidget(widget)

4e72ad4418e28e9ce6f589265b6a0589a053abde
Volkov <seishikage@gmail.com>
2021-03-26 22:16:03 +0300

    Добавлены игры в список игр для выбора

--- modules/BoardGamesCreate.py
+++ modules/BoardGamesCreate.py
@@ -18,12 +18,33 @@ class BoardGamesCreate(QMainWindow):
         self.general_layout.addWidget(self.create_bestiary)
         self.create_bestiary.clicked.connect(self.action_create_bestiary)
 
+        self.create_war_chest = QPushButton("Сундук Войны")
+        self.general_layout.addWidget(self.create_war_chest)
+        self.create_war_chest.clicked.connect(self.action_create_war_chest)
+
+        self.create_undaunted = QPushButton("Неустрашимые")
+        self.general_layout.addWidget(self.create_undaunted)
+        self.create_undaunted.clicked.connect(self.action_create_undaunted)
+
+        self.create_dice_throne = QPushButton("Трон Кубов")
+        self.general_layout.addWidget(self.create_dice_throne)
+        self.create_dice_throne.clicked.connect(self.action_create_dice_throne)
+
         widget = QWidget()
         widget.setLayout(self.general_layout)
 
         self.setCentralWidget(widget)
 
     def action_create_bestiary(self):
+        self.close()
+
+    def action_create_war_chest(self):
+        self.close()
+
+    def action_create_undaunted(self):
+        self.close()
+
+    def action_create_dice_throne(self):
         self.close()
 
 

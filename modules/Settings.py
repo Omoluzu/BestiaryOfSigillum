9892c5ba697a52bbf8ea57d11590b7641d6bd57e
volkov.al@tecomgroup.ru <seishikage@gmail.com>
2021-11-07 18:07:38 +0300

    В виджет настроек подставляются значение из файла конфигов

--- modules/Settings.py
+++ modules/Settings.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5.QtWidgets import *
+
 from wrapperQWidget5.WrapperWidget import wrapper_widget
+from modules.configControl.configControl import Config
 
 
 class Settings(QDialog):
@@ -11,17 +13,19 @@ class Settings(QDialog):
     def __init__(self):
         super().__init__()
 
+        self.settings = Config()
+
         group_address = QGroupBox("Адрес сервера: ")
         layout_address = QHBoxLayout()
         group_address.setLayout(layout_address)
-        text_address = QLineEdit()
-        layout_address.addWidget(text_address)
+        self.text_address = QLineEdit(self.settings.get("SERVER", "address"))
+        layout_address.addWidget(self.text_address)
 
         group_port = QGroupBox("Порт сервера: ")
         layout_port = QHBoxLayout()
         group_port.setLayout(layout_port)
-        text_port = QLineEdit()
-        layout_port.addWidget(text_port)
+        self.text_port = QLineEdit(self.settings.get("SERVER", "port"))
+        layout_port.addWidget(self.text_port)
 
         btn_save = QPushButton("Созранить")
         btn_save.clicked.connect(self.action_save_setting)

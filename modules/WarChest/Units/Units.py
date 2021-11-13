efa45d578587b06437fed6da8771f473797c0bea
volkov.al@tecomgroup.ru <seishikage@gmail.com>
2021-11-13 10:26:34 +0300

    ListUnits выведен в файл __init__

--- modules/WarChest/Units/Units.py
+++ modules/WarChest/Units/Units.py
@@ -165,25 +165,3 @@ class Chaplain(Units):
 
     def translate(self):
         return "Капеланы"
-
-
-ListUnits = {
-    "Crossbowman": Crossbowman,
-    "RoyalGuard": RoyalGuard,
-    "Mercenaries": Mercenaries,
-    "Pikemen": Pikemen,
-    "Marshal": Marshal,
-    "Cavalry": Cavalry,
-    "Spearmen": Spearmen,
-    "StandardBearer": StandardBearer,
-    "LightCavalry": LightCavalry,
-    "Berserkers": Berserkers,
-    "Archers": Archers,
-    "Infantry": Infantry,
-    "Knights": Knights,
-    "Scouts": Scouts,
-    "Swordsmen": Swordsmen,
-    "Chaplain": Chaplain
-}
-
-
--- modules/WarChest/Units/__init__.py
+++ modules/WarChest/Units/__init__.py
@@ -1,1 +1,21 @@
-from modules.WarChest.Units.Units import ListUnits
+from modules.WarChest.Units.Units import *
+
+ListUnits = {
+    "Crossbowman": Crossbowman,
+    "RoyalGuard": RoyalGuard,
+    "Mercenaries": Mercenaries,
+    "Pikemen": Pikemen,
+    "Marshal": Marshal,
+    "Cavalry": Cavalry,
+    "Spearmen": Spearmen,
+    "StandardBearer": StandardBearer,
+    "LightCavalry": LightCavalry,
+    "Berserkers": Berserkers,
+    "Archers": Archers,
+    "Infantry": Infantry,
+    "Knights": Knights,
+    "Scouts": Scouts,
+    "Swordsmen": Swordsmen,
+    "Chaplain": Chaplain
+}
+

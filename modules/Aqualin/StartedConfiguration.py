f84c6aa8576eb2c47415ea192aa7455581ccd4ac
volkov.al@tecomgroup.ru <seishikage@gmail.com>
2022-05-12 20:19:43 +0300

    Aqualin - Выбор типа собираемой комбинации

--- modules/Aqualin/StartedConfiguration.py
+++ modules/Aqualin/StartedConfiguration.py
@@ -19,12 +19,16 @@ def started_configuration(data):
         'game_info': None
     }
     """
-    users = copy.deepcopy(data['users'])
-    active_player = users.pop(random.randint(0, 1))
+    select_active_player = random.randint(0, 1)
+    select_int_type_user = random.randint(0, 1)
 
     game_info = {
-        "active_player": active_player,
-        "initiative": active_player,
+        "active_player": data['users'][select_active_player],
+        "initiative": data['users'][select_active_player],
+        "type_users": {
+            "color": data['users'][select_int_type_user],
+            "dweller": data['users'][1 - select_int_type_user]
+        }
     }
 
     return game_info

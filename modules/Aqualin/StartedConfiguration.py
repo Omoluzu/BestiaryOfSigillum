ec492b21f6a93e656ff6aa249f7f483012f8d8b7
volkov.al@tecomgroup.ru <seishikage@gmail.com>
2022-05-12 20:09:13 +0300

    Aqualin - Выбор активного игрока

--- modules/Aqualin/StartedConfiguration.py
+++ modules/Aqualin/StartedConfiguration.py
@@ -1,8 +1,30 @@
-
+import copy
+import random
 
 def started_configuration(data):
     """
     Стартовая конфгурация игры Аквалин
     """
+    """
+    {
+        'create_user': 'Omoluzu', 
+        'games_config': {
+            'select_players': 2, 
+            'select_unit': 'random'
+        }, 
+        'games': 'aqualin', 
+        'users': ['Omoluzu', 'Hokage'], 
+        'command': 'game_info', 
+        'game_id': 5, 
+        'game_info': None
+    }
+    """
+    users = copy.deepcopy(data['users'])
+    active_player = users.pop(random.randint(0, 1))
 
-    return data
+    game_info = {
+        "active_player": active_player,
+        "initiative": active_player,
+    }
+
+    return game_info

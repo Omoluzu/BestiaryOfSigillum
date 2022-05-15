54c4cff8a6ca43b8d6123de77fde10387aa252c1
volkov.al@tecomgroup.ru <seishikage@gmail.com>
2022-05-15 22:57:19 +0300

    Aqualin - Активация юнитов

--- modules/Aqualin/Scene/UnitTile.py
+++ modules/Aqualin/Scene/UnitTile.py
@@ -2,6 +2,8 @@
 Тайлы юнитов
 """
 
+from PyQt5.QtGui import QPen, QColor
+
 from wrapperQWidget5.modules.scene.RectangleScene_new import RectangleScene
 from ..Settings import SIZE
 
@@ -21,4 +23,18 @@ class UnitTile(RectangleScene):
 
     def set_image(self, path, bias=(0, 0)):
         super().set_image(path, bias=(5, 5))
+
+    def activated(self):
+        if self.scene.active:
+            self.scene.active.deactivated()
+
+        if self.status == 'field' and self.scene.check_move:
+            pass
+        else:
+            self.scene.active = self
+            self.setPen(QPen(QColor("Red"), 6))
+
+    def deactivated(self):
+        self.scene.active = None
+        self.setPen(QPen(QColor('Black'), 1))
 

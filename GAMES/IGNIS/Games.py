e9be5ee32e21222bd4cd78d1f65780ab3fbc8e79
volkov.al <volkov.al@tecomgroup.ru>
2023-04-19 19:06:15 +0300

    ignis - Подключил общий WrapperGames

--- GAMES/IGNIS/Games.py
+++ GAMES/IGNIS/Games.py
@@ -1,63 +1,12 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget
 from .Scene import IgnisScene
+from src.wrapper.view import WrapperGames
 
-__version__ = "1.0.0"
-
-
-class WrapperGames(QMainWindow):
-    __scene__ = None
-    title = "WrapperGames"
-    version_game = "0.0.0"
-
-    def __init__(self, client, data):
-        super().__init__()
-
-        self.client = client
-        self.data = data
-
-        self.setWindowTitle(f"{self.title} ({self.version_game})")
-
-        self.widget = QWidget(self)
-        self.setCentralWidget(self.widget)
-        self.scene = self.__scene__(self, size=(1110, 600))  # Fixme, size не должен быть захардкожен в WrapperGames
-
-    def start(self, close=True):
-        """ Активация приложения """
-        self.client.action = self
-        if close:
-            self.client.boardgames_list.close()
-
-        self.show()
-
-    def data_received(self, data: dict) -> None:
-        if self.data['game_id'] == data['game_id']:
-            self.get_data(data)
-
-    def get_data(self, data: dict):
-        pass
-
-    def send_data(self, command, test=False):
-        self.client.send_data({
-            'test': test,
-            'command': 'game_update',
-            'user': self.client.user,
-            'games': self.data['games'],
-            'game_id': self.data['game_id'],
-            'game_command': command
-        })
-
-    def set_hide(self):
-        self.setVisible(False)
-
-    def show_app(self):
-        self.setVisible(True)
-
-    def close_app(self):
-        self.close()
+__version__ = "1.0.1"
 
 
 class Games(WrapperGames):
     __scene__ = IgnisScene
+    __scene__size__ = (1110, 600)
     title = "Игнис"
     version_game = __version__
 

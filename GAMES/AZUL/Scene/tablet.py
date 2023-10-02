e7a190db85921c6f564d7382599c5d53ccafa921
volkov.al <volkov.al@tecomgroup.ru>
2023-10-02 18:30:51 +0300

    AZUL - Заполнены линии шаблона

--- GAMES/AZUL/Scene/tablet.py
+++ GAMES/AZUL/Scene/tablet.py
@@ -14,12 +14,33 @@ class PatternLine:
         self.pattern_lines = pattern_lines
         self.scene = pattern_lines.scene
         self.count = count
+
         self.draw()
 
+    @property
+    def start_point(self) -> tuple:
+        return (
+            self.pattern_lines.start_point_x - 12,
+            self.pattern_lines.start_point_y
+        )
+
+    @property
+    def bias_y(self) -> int:
+        return {
+            1: -2.4,
+            2: -1.2,
+            3: 0,
+            4: 1.2,
+            5: 2.4,
+        }.get(self.count)
+
     def draw(self):
-        Pattern(self.scene, point=self.pattern_lines.start_point)
-        Pattern(self.scene, point=self.pattern_lines.start_point, bias=(1.2, 0))
-        Pattern(self.scene, point=self.pattern_lines.start_point, bias=(2.4, 0))
+        for bias_x in [2.4, 1.2, 0, -1.2, -2.4][:self.count]:
+            Pattern(
+                self.scene,
+                point=self.start_point,
+                bias=(bias_x, self.bias_y)
+            )
 
 
 class PatternLines(RectangleElementScene):
@@ -28,11 +49,15 @@ class PatternLines(RectangleElementScene):
     def __init__(self, tablet, point, *args, **kwargs):
         self.height = tablet.height - 10
         self.width = tablet.width / 2
-        x = point[0] - (self.width / 2) + 10
+        x = point[0] - (self.width / 2) + 7
         y = point[1]
         super().__init__(scene=tablet.scene, point=(x, y), *args, **kwargs)
 
-        # pattern_lines_3 = PatternLine(pattern_lines=self, count=3)
+        pattern_lines_1 = PatternLine(pattern_lines=self, count=1)
+        pattern_lines_2 = PatternLine(pattern_lines=self, count=2)
+        pattern_lines_3 = PatternLine(pattern_lines=self, count=3)
+        pattern_lines_4 = PatternLine(pattern_lines=self, count=4)
+        pattern_lines_5 = PatternLine(pattern_lines=self, count=5)
 
 
 class Tablet(RectangleElementScene):

71752409f5b69a4ce587558c03a2952a2ad61c67
volkov.al <volkov.al@tecomgroup.ru>
2023-10-24 11:40:54 +0300

    engine - Скрипт создания новых игр генерирует файл Games

--- script/new_game.py
+++ script/new_game.py
@@ -10,6 +10,28 @@ class {title_name}Scene(Scene):
 """
 
 
+games_file = """from src.wrapper.view import WrapperGames
+from .Scene.Scene import {title_name}Scene
+
+__version__ = "1.0.0"
+
+
+class {title_name}Games(WrapperGames):
+    __scene__ = {title_name}Scene
+    __scene__size__ = (1920, 1080)
+    title = "{name_game}"
+    version_game = __version__
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setFixedSize(*self.__scene__size__)
+        self.setContentsMargins(0, 0, 0, 0)
+
+    def get_data(self, data: dict) -> None:
+        print(data)
+"""
+
+
 def new_game(name_game):
     upper_name = name_game.upper().replace(' ', '_')
     title_name = name_game.title().replace(' ', '')
@@ -28,6 +50,12 @@ def new_game(name_game):
             os.path.join(path_scene, 'Scene.py'), 'w', encoding='utf-8'
     ) as file:
         file.write(scene_file.format(title_name=title_name))
+
+    with open(
+            os.path.join(path, 'Games.py'), 'w', encoding='utf-8'
+    ) as file:
+        file.write(games_file.format(
+            title_name=title_name, name_game=name_game))
 
 
 if __name__ == '__main__':

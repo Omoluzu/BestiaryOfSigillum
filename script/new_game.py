85ebd1fcd6acecb42d99207c559c33b6f970fc0d
volkov.al <volkov.al@tecomgroup.ru>
2023-10-24 09:36:14 +0300

    engine - Скрипт создания новых игр генерирует файл Сцены

--- script/new_game.py
+++ script/new_game.py
@@ -2,16 +2,32 @@ import os
 from pathlib import Path
 
 
+scene_file = """from wrapperQWidget5.modules.scene.Scene import Scene
+
+
+class {title_name}Scene(Scene):
+    pass
+"""
+
+
 def new_game(name_game):
     upper_name = name_game.upper().replace(' ', '_')
+    title_name = name_game.title().replace(' ', '')
+
     path = os.path.join(
         Path(__file__).resolve().parents[1], 'Games', upper_name)
+    path_scene = os.path.join(path, 'Scene')
 
     if os.path.exists(path):
         print(f"Игра и именем {name_game}, уже присутствует в системе")
         exit(1)
 
     os.mkdir(path)
+    os.mkdir(path_scene)
+    with open(
+            os.path.join(path_scene, 'Scene.py'), 'w', encoding='utf-8'
+    ) as file:
+        file.write(scene_file.format(title_name=title_name))
 
 
 if __name__ == '__main__':

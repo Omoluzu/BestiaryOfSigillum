4a1f1034f8e089a7726261f665a070543ab37b01
volkov.al <volkov.al@tecomgroup.ru>
2024-02-06 13:31:20 +0300

    AZUL - Раскраска шаблонов для размещения новых тайлов

--- GAMES/AZUL/Scene/Tablet/Pattern/pattern.py
+++ GAMES/AZUL/Scene/Tablet/Pattern/pattern.py
@@ -8,3 +8,9 @@ from src.wrapper.element import SquareElementScene
 
 class Pattern(SquareElementScene):
     size = 50
+
+    def get_active(self):
+        self.set_color(color="green")
+
+    def get_deactivate(self):
+        self.set_color()
--- GAMES/AZUL/Scene/Tablet/Pattern/pattern_line.py
+++ GAMES/AZUL/Scene/Tablet/Pattern/pattern_line.py
@@ -6,10 +6,18 @@ from .pattern import Pattern
 
 
 class PatternLine:
+    """
+
+    Args:
+        count - Длина линии от 1 до 5.
+        pattern_tiles - Список шаблонов или тайлов для размещения
+            или хранения информации о тайлах.
+    """
     def __init__(self, pattern_lines: 'PatternLines', count: int):
         self.pattern_lines = pattern_lines
         self.scene = pattern_lines.scene
         self.count = count
+        self.pattern_tiles: list[Pattern] = []
 
         self.draw()
 
@@ -32,11 +40,12 @@ class PatternLine:
 
     def draw(self):
         for bias_x in [2.4, 1.2, 0, -1.2, -2.4][:self.count]:
-            Pattern(
+            pattern = Pattern(
                 self.scene,
                 point=self.start_point,
                 bias=(bias_x, self.bias_y)
             )
+            self.pattern_tiles.append(pattern)
 
     def show_me_put_tile(self, color):
         """Отрисовка тайлов куда можно положить разместить тайл в Линии шаблона
@@ -44,4 +53,5 @@ class PatternLine:
         Args:
             color: Выбранный цвет тайла который планируется для размещения.
         """
-        print(f"show_me_put_tile: {color}")
+        for pattern in self.pattern_tiles:
+            pattern.get_active()
